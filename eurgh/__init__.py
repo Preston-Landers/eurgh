2c676612e0c8973ee119f5fd7c564469f47d5b4a
Preston Landers <planders@utexas.edu>
2014-06-15 16:03:09 -0500

    Handle the TranslateArray API call.

--- eurgh/__init__.py
+++ eurgh/__init__.py
@@ -6,14 +6,18 @@
 Eurgh - a translation utility using Microsoft Translate API
 """
 
-from configparser import ConfigParser
+import re
 import time
+from configparser import ConfigParser
 from logging import getLogger
 from urllib.request import Request, urlopen
 from urllib.parse import urlencode
 from urllib.error import HTTPError
 from json import loads
 import xml.etree.ElementTree as ET
+
+from eurgh.languages import LANGUAGES
+
 
 __author__ = 'Preston Landers (planders@gmail.com)'
 __copyright__ = 'Copyright (c) 2014 Preston Landers'
@@ -28,6 +32,9 @@ class Eurgh(object):
 
     AUTH_URL = "https://datamarket.accesscontrol.windows.net/v2/OAuth2-13"
 
+    # Max size of string array for TranslateArray
+    MAX_API_ARRAY = 2000
+
     def __init__(self, config_file):
         self.config = config = ConfigParser()
         config.read(config_file)
@@ -37,8 +44,14 @@ class Eurgh(object):
         self.from_lang = config.get("translate", "from_lang")
         self.to_langs = config.get("translate", "to_lang").split(" ")
 
+        for lang in self.to_langs:
+            if lang not in LANGUAGES:
+                raise ValueError("Unsupported language: %s" % (lang,))
+
         self.client_id = config.get("secrets", "client_id")
         self.client_secret = config.get("secrets", "client_secret")
+
+        self.api_category = config.get("translate", "category", fallback="general")
         return
 
     @property
@@ -77,17 +90,77 @@ class Eurgh(object):
         if to_lang is None:
             to_lang = self.to_langs[0]
 
-        # url = BASE_API + "TranslateArray?"
         url = self.BASE_API + "Translate?"
-        data = {
-            "from": from_lang,
-            "to": to_lang,
-            # "texts": dumps(tr_strings),
-            "text": tr_string,
-        }
-        params = urlencode(data)
-
+        # Must be in this order!
+        params = urlencode([
+            ("text", tr_string),
+            ("from", from_lang),
+            ("to", to_lang),
+            ("contentType", "text/plain"),
+            ("category", self.api_category),
+        ])
         request = Request(url + params)
+        result = self.run_request(request)
+        return self.deserialize(result)
+
+    def translate_strings(self, str_array, from_lang=None, to_lang=None):
+        if len(str_array) > self.MAX_API_ARRAY:
+            raise ValueError(
+                "List is too big to translate, %s is greater than %s" % (len(str_array), self.MAX_API_ARRAY))
+        if from_lang is None:
+            from_lang = self.from_lang
+        if to_lang is None:
+            to_lang = self.to_langs[0]
+        url = self.BASE_API + "TranslateArray?"
+        # Must be in this order!
+        data = [("from", from_lang)]
+        data.extend([("texts", thing) for thing in str_array])
+        data.extend([("to", to_lang)])
+
+        strings_enc = "\n".join([self.serialize(thing) for thing in str_array])
+        category = self.api_category
+        content_type = "text/plain"
+
+        data = """\
+<TranslateArrayRequest>
+  <AppId />
+  <From>%(from_lang)s</From>
+  <Options>
+    <Category xmlns="http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2" >%(category)s</Category>
+    <ContentType xmlns="http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2">%(content_type)s</ContentType>
+    <ReservedFlags xmlns="http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2" />
+    <State xmlns="http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2" />
+    <Uri xmlns="http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2" />
+    <User xmlns="http://schemas.datacontract.org/2004/07/Microsoft.MT.Web.Service.V2" />
+  </Options>
+  <Texts>
+    %(strings_enc)s
+  </Texts>
+  <To>%(to_lang)s</To>
+</TranslateArrayRequest>""" % locals()
+
+        data_bytes = bytes(data.encode("utf-8"))
+        request = Request(url, data_bytes, method="POST")
+        request.add_header("Content-Type", "text/xml")
+        result = self.run_request(request)
+        result_list = self.deserialize_array(result)
+        result_dict = self.simplify_array_result(str_array, result_list)
+        return result_list
+
+    @staticmethod
+    def simplify_array_result(orig_array, result_list):
+        res = {}
+        for i in range(len(orig_array)):
+            orig_str = orig_array[i]
+            result_entry = result_list[i]
+            res[orig_str] = result_entry["TranslatedText"]
+        return res
+
+    @staticmethod
+    def serialize(thing):
+        return '<string xmlns="http://schemas.microsoft.com/2003/10/Serialization/Arrays">%s</string>' % (thing,)
+
+    def run_request(self, request):
         request.add_header("Authorization", "Bearer %s" % (self.access_token['access_token'],))
         try:
             response = urlopen(request)
@@ -97,13 +170,28 @@ class Eurgh(object):
             # return 1
             raise e
         result = response.read().decode("utf-8")
-        # print(result)
-        return self.deserialize(result)
+        return result
 
     @staticmethod
     def deserialize(result):
         tree = ET.fromstring(result)
         return tree.text
+
+    @staticmethod
+    def deserialize_array(array_response):
+        tree = ET.fromstring(array_response)
+        res = []
+        for response in tree:
+            d = {}
+            for part in response:
+                tag = strip_tag(part.tag)
+                d[tag] = part.text
+            res.append(d)
+        return res
+
+
+def strip_tag(atag):
+    return re.sub(r"{.*?}", "", atag)
 
 
 def test():
@@ -112,9 +200,10 @@ def test():
         "Goodbye."
     )
     eurgh = Eurgh("local.ini")
-    for tr_str in test_strings:
-        res = eurgh.translate_string(tr_str)
-        print("%s ==> %s" % (tr_str, res))
+    # for tr_str in test_strings:
+    # res = eurgh.translate_string(tr_str)
+    # print("%s ==> %s" % (tr_str, res))
+    print(eurgh.translate_strings(test_strings))
 
 
 if __name__ == "__main__":
---
+++ eurgh/languages.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vim: fileencoding=utf-8 tabstop=4 expandtab shiftwidth=4
+
+"""
+Contains supported languages.
+
+Supported languages as of Feb 2014:
+http://msdn.microsoft.com/en-us/library/hh456380.aspx
+
+"""
+
+# : Supported languages as of Feb 2014:
+#: http://msdn.microsoft.com/en-us/library/hh456380.aspx
+LANGUAGES = {
+    "ar": "Arabic",
+    "bg": "Bulgarian",
+    "ca": "Catalan",
+    "zh-CHS": "Chinese Simplified",
+    "zh-CHT": "Chinese Traditional",
+    "cs": "Czech",
+    "da": "Danish",
+    "nl": "Dutch",
+    "en": "English",
+    "et": "Estonian",
+    "fi": "Finnish",
+    "fr": "French",
+    "de": "German",
+    "el": "Greek",
+    "ht": "Haitian Creole",
+    "he": "Hebrew",
+    "hi": "Hindi",
+    "mww": "Hmong Daw",
+    "hu": "Hungarian",
+    "id": "Indonesian",
+    "it": "Italian",
+    "ja": "Japanese",
+    "tlh": "Klingon",
+    "tlh - Qaak": "Klingon (pIqaD)",
+    "ko": "Korean",
+    "lv": "Latvian",
+    "lt": "Lithuanian",
+    "ms": "Malay",
+    "mt": "Maltese",
+    "no": "Norwegian",
+    "fa": "Persian",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "ro": "Romanian",
+    "ru": "Russian",
+    "sk": "Slovak",
+    "sl": "Slovenian",
+    "es": "Spanish",
+    "sv": "Swedish",
+    "th": "Thai",
+    "tr": "Turkish",
+    "uk": "Ukrainian",
+    "ur": "Urdu",
+    "vi": "Vietnamese",
+    "cy": "Welsh",
+}
