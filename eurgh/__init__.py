23d1eb12d0d07c767682766cdf669c77d2bf3bd4
Preston Landers <planders@utexas.edu>
2014-06-15 13:24:42 -0500

    Initial version.

---
+++ eurgh/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vim: fileencoding=utf-8 tabstop=4 expandtab shiftwidth=4
+
+
+"""
+
+"""
+
+from logging import getLogger
+
+__author__ = 'Preston Landers (planders@gmail.com)'
+__copyright__ = 'Copyright (c) 2014 Preston Landers'
+__license__ = 'Proprietary'
+__version__ = '0.1'
+
+log = getLogger(__name__)
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vim: fileencoding=utf-8 tabstop=4 expandtab shiftwidth=4
+
+"""
+Eurgh translator
+"""
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+__author__ = 'Preston Landers (planders@gmail.com)'
+__copyright__ = 'Copyright (c) 2014 Preston Landers'
+__license__ = 'Proprietary'
+
+requires = [
+]
+
+setup(
+    name='eurgh',
+    version='0.1',
+    description='eurgh',
+    long_description=README + '\n\n' + CHANGES,
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Topic :: Software Development :: Internationalization",
+        "Topic :: Software Development :: Localization",
+        "Topic :: Text Processing :: Linguistic",
+        "Programming Language :: Python",
+    ],
+    author='Preston Landers',
+    author_email='planders@gmail.com',
+    url='https://bitbucket.org/planders/eurgh',
+    keywords='translation',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite='eurgh.tests',
+    install_requires=requires,
+)
